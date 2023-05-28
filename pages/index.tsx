77094c8fe343bf3f3bf36f7c9e359b2ac5d35d3c
prrockzed <prrockzed@pm.me>
2023-05-29 00:09:41 +0530

    Converted to typescript and added css to the project

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 export default function Home() {
   return (
     <>
-      <h1>Hi This is the Rock, Paper, Scissor, Spock, Lizard Game</h1>
+      <h1>Hi This is the Rock, Paper, Scissor, Spock, Lizard Game in Typescript</h1>
     </>
   )
 }
