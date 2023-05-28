9cd1efbb90cb0af70275052869f60ecc6c6351bb
prrockzed <prrockzed@pm.me>
2023-05-29 00:46:57 +0530

    Made the footer of the game

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,25 @@
 import Head from 'next/head'
+import { useState } from 'react'
 
 export default function Home() {
+  const [advancedMode, setAdvancedMode] = useState(false)
+
+  const changeMode = () => {
+    setAdvancedMode(!advancedMode)
+  }
+
   return (
-    <>
-      <h1>Hi This is the Rock, Paper, Scissor, Spock, Lizard Game in Typescript</h1>
-    </>
+    <div>
+      <Head>
+        <title>RPSSL</title>
+      </Head>
+      <footer>
+        <div onClick={changeMode} className='modes'>
+          {advancedMode ? 'Advanced' : 'Normal'} Mode
+        </div>
+
+        <div className='rules'>Rules</div>
+      </footer>
+    </div>
   )
 }
